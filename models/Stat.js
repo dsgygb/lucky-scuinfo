037c9cd857e1f314ca85202ac45dd8a66b92f7a0
小明plus <xiaomingplus@qq.com>
2016-05-24 00:32:14 +0800

    for update

--- models/Stat.js
+++ models/Stat.js
@@ -6,7 +6,7 @@ var StatSchema = new Schema({
     _id: {type: String ,index: true},
     views:{type:Number,default:0},
     groups:{type:Number,default:0},
-    lucky_time:{type:Number,default:1447768800},
+    lucky_time:{type:Number,default:1473350400},
     create_at:Number
 });
 
